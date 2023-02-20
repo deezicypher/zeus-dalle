90e84e2debfa0ad668900e7cc91096df36fc4a72
deezitheviper <amuhdavid@gmail.com>
2023-02-20 19:18:44 +0100

    up

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,26 @@
 import React,{useState,useEffect} from 'react'
 import { Loader, Card, FormField } from '../components'
 
+const RenderCards = ({data, title}) => {
+    if(data?.length > 0){
+         return data.map((post)=> 
+            <Card 
+                key={post._id} {...post} 
+            />
+    )
+    }
+    return <h2 className="mt-5 font-bold text-[#a47e56] text-xl uppercase">
+        {title}
+    </h2>
+}
 
 
 const Home = () => {
     const [loading, setLoading] = useState(false)
     const [allposts, setAllPosts] = useState(null)
+    const [searchText, setSearchText] = useState("")
+
+
 
   return (
     <section className='max-w-7xl mx-auto'>
@@ -16,6 +31,36 @@ const Home = () => {
             <p className='mt-2 text-[#666e75] text-[14px] max-w-[500px] '>
                 Browse Through a collection of imaginationative AI image generation using DALL-E AI
             </p>
+        </div>
+        <div className='mt-16 '>
+            <FormField/>
+        </div>
+        <div className='mt-10'>
+            {loading?
+            <div className='flex justify-center'><Loader/> </div>
+:
+<>
+{searchText && (
+<h2 className='font-medium text-[#666e75] text-xl mb-3'>
+    Showing results for <span className='text-[#222328]'>{searchText}</span>
+</h2>
+)}
+<div className='grid lg:grid-cols-4 sm:grid-cols-3 xsm:grid-cols-2 grid-cols-1 ap-3'>
+    {searchText?
+    <RenderCards 
+    data={[]} 
+    title="No search results found"
+    />
+    :
+    <RenderCards 
+     data={[]}
+     title="No posts found"
+     />
+    }
+</div>
+</>
+        }
+
         </div>
     </section>
   )
