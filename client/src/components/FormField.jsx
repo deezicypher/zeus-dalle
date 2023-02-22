0dd8cffed778054e42da3c0efc8578414d9e654b
deezitheviper <amuhdavid@gmail.com>
2023-02-22 11:00:39 +0100

    formfield

--- client/src/components/FormField.jsx
+++ client/src/components/FormField.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
 
-const FormField = () => {
+const FormField = ({labelName, type, name, value, handleChange, isSupriseMe, handleSupriseMe, placeholder,}) => {
   return (
-    <div>FormField</div>
+    <div>
+        <div className='flex items-center gap-2 mb-2'>
+            <label 
+            htmlFor={name}
+            className="block text-sm font-medium text-gray-900"
+            >
+            {labelName}
+            </label>
+            {!isSupriseMe &&
+            <button 
+            type='button' 
+            onClick={handleSupriseMe} 
+            className="font-semibold text-xs bg-[#ECECF1] py-1 px-2 rounded-[5px] text-black" 
+>Suprise Me</button>
+            }
+        </div>
+        <input 
+            type={type}
+            id={name}
+            name={name}
+            placeholder={placeholder}
+            value={value}
+            onChange={handleChange}
+            required
+            className='bg-gray-50 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#46acff] focus:border-[#46acff] outline-none block w-full p-3'
+        />
+
+    </div>
   )
 }
 
--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import {useState} from 'react'
+import { useNavigate } from 'react-router-dom'
+import { preview } from '../assets'
+import { FormField, Loader } from '../components'
+import { getRandomPrompt } from '../utils'
+
 
 const CreatePost = () => {
+  const navigate = useNavigate();
+  const [form, setForm] = useState({
+    name:"", 
+    prompt:"",
+    photo:"",
+
+  })
+
+  const [generatingImg, setGeneratingImg] = useState(false);
+  const [loading, setLoading] = useState(false);
+  
+
   return (
-    <div>CreatePost</div>
+    <section className='max-w-7xl mx-auto'>
+         <div>
+            <h1 className='font-extrabold text-[32px]'>
+                The Community Showcase
+            </h1>
+            <p className='mt-2 text-[#666e75] text-[14px] max-w-[500px] '>
+                Browse Through a collection of imaginationative AI image generation using DALL-E AI
+            </p>
+        </div>
+    </section>
   )
 }
 
