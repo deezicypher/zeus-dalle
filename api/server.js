3bf92c459d041d10c467c9120398e2b9004ec5d4
deezitheviper <amuhdavid@gmail.com>
2023-02-23 02:09:57 +0100

    update on api

--- api/server.js
+++ api/server.js
@@ -1,16 +1,18 @@
 import express from "express";
 import * as dotenv from "dotenv";
 import cors from 'cors';
-
+import postRoutes from './routes/post.js';
+import dalleRoutes  from './routes/dalle.js';
 
 dotenv.config()
 const app = express();
 app.use(cors())
 app.use(express.json({limit: '50mb'}))
 
-app.get('/', async (req, res) => {
-    res.send('Server')
-})
+app.use('/api/v1/post',postRoutes);
+app.use('/api/v1/dalle',dalleRoutes);
+
+
 
 const startServer = async () => {
     app.listen(8080, ()=> console.log('Server has started on port 8080'))
